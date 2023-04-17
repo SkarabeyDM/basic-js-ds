6cf0c99e5c9fed7dfc4f69477e237e9841d05576
Skarabey DM <mechanoid0415@gmail.com>
2023-04-18 00:52:54 +0300

    refactor: commented tests

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -143,25 +143,25 @@ class BinarySearchTree
 }
 
 
-const tree = new BinarySearchTree()
-tree.add(9);
-tree.add(14);
-tree.add(2);
-tree.add(6);
-tree.add(128);
-tree.add(8);
-tree.add(31);
-tree.add(54);
-tree.add(1);
-/* tree.render() */
-tree.remove(14);
-tree.remove(8);
-tree.remove(9);
+// const tree = new BinarySearchTree()
+// tree.add(9);
+// tree.add(14);
+// tree.add(2);
+// tree.add(6);
+// tree.add(128);
+// tree.add(8);
+// tree.add(31);
+// tree.add(54);
+// tree.add(1);
+// tree.render()
+// tree.remove(14);
+// tree.remove(8);
+// tree.remove(9);
 
 //let output = tree.remove(9)//root()
 
 
-console.log(/* tree.max(), tree.min(), tree.has(128) */ tree.has(8))
+//console.log(tree.max(), tree.min(), tree.has(128) tree.has(8))
 
 module.exports = {
   BinarySearchTree
